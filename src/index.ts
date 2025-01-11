167412843ff95242b3ab010bdb4f9124cd00eada
devconet <42451250+devconet@users.noreply.github.com>
2025-01-11 23:27:48 +0100

    Moved hardcoded mnemonic to the secrets.json file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
 import * as bip39 from 'bip39';
 import { getAddressFromPublicKey } from '@solana/web3.js';
 import { createKeyPairFromPrivateKeyBytes } from '@solana/keys';
 import { derivePath } from "ed25519-hd-key"; 
 
 const c = require('ansi-colors');
+
+function getMnemonicFromFile(): string {
+    const secretsPath = path.join(__dirname, '../secrets.json');
+
+    if (!fs.existsSync(secretsPath)) {
+        throw new Error('secrets.json file not found');
+    }
+
+    const rawData = fs.readFileSync(secretsPath, 'utf-8');
+    const secrets = JSON.parse(rawData);
+
+    if (!secrets.mnemonic) {
+        throw new Error('Mnemonic not found in secrets.json');
+    }
+
+    return secrets.mnemonic;
+}
 
 async function createWalletFromSeed(mnemonic:string) {
     const seed = bip39.mnemonicToSeedSync(mnemonic);
@@ -16,8 +36,8 @@ async function createWalletFromSeed(mnemonic:string) {
 
 async function main() {
 
-    //Mnemonic created with Solana Cli command "solana-keygen new --no-outfile"
-    const seedPhrase = '';
+    //Mnemonic stored in secrets.json was created with Solana Cli command "solana-keygen new --no-outfile"
+    const seedPhrase = getMnemonicFromFile();
 
     const walletAddress = await createWalletFromSeed(seedPhrase);
     console.log('Wallet address: '+c.blue(walletAddress));
